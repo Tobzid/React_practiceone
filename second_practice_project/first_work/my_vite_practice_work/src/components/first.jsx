16953919ebd5fef1299085a049ce5cad44292a4f
Tobzid <tobzid2013@gmail.com>
2025-07-09 08:25:18 +0100

    This folder has my component file

---
+++ second_practice_project/first_work/my_vite_practice_work/src/components/first.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from "react";
+
+function TheState(){
+
+    const [count, setCount] = useState(0);
+
+    return(
+        <>
+        <div>
+
+<h1>This is my account.</h1>
+        </div>
+         </>
+    )
+}
+export default TheState;
