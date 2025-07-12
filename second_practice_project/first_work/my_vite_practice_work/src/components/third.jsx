4496dbd973306eda7bf8de8c8646b47cb02a3b34
Tobzid <tobzid2013@gmail.com>
2025-07-12 06:01:17 +0100

    useState Object Example

--- second_practice_project/first_work/my_vite_practice_work/src/components/third.jsx
+++ second_practice_project/first_work/my_vite_practice_work/src/components/third.jsx
@@ -3,23 +3,34 @@ import { useState } from "react";
 
 function UseStateObject(){
 
-const [name, setName] = useState("Peter");
-const [age, setAge] = useState(24);
-const [hobby, setHobby] = useState("Read books");
+    const [person, setPerson] = useState(
+        {name: "Peter",
+            age: 24,
+            hobby: "Read books"
+        }
+    );
+
+
+
+// const [name, setName] = useState("Peter");
+// const [age, setAge] = useState(24);
+// const [hobby, setHobby] = useState("Read books");
 
 const displayPerson = ()=>{
-    setName("John");
-    setAge(28);
-    setHobby("Scream at the computer");
+    // setName("John");
+    // setAge(28);
+    // setHobby("Scream at the computer");
+    setPerson({name: "John", age: 28, hobby : "Scream At The Computer"});
+
 }
 return(
     <>
     <div className="objUseCont">
 <h4>useState Object example</h4>
 
-<h3>{name}</h3>
-<h3>{age}</h3>
-<h4> Enjoys: {hobby}</h4>
+<h3>{person.name}</h3>
+<h3>{person.age}</h3>
+<h4> Enjoys: {person.hobby}</h4>
 <button className="btn" onClick={displayPerson}>Change to John</button>
     </div>
     
