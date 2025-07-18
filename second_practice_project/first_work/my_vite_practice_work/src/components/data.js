33b4e2ecdeb40c88c163d160e142fe91ce1cb230
Tobzid <tobzid2013@gmail.com>
2025-07-18 05:53:17 +0100

    data updates

---
+++ second_practice_project/first_work/my_vite_practice_work/src/components/data.js
@@ -0,0 +1,13 @@
+export const data = [
+    {id:1, name: "Peter"},
+    {id:2, name: "Paul"},
+    {id:3, name: "James"},
+    {id:4, name: "Joe"}
+];
+
+export const people = [
+    { id: 1, name: "Bob", nickname: "Banger Bongo"},
+    { id: 2, name: "Bran", nickname: ""}
+];
+
+export const someValues = []
