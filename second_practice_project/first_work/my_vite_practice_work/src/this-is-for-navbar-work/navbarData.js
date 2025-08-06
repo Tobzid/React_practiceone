450ee4f41d1c08698077610c182f764354ff0388
Tobzid <tobzid2013@gmail.com>
2025-08-06 07:13:05 +0100

    The data file

---
+++ second_practice_project/first_work/my_vite_practice_work/src/this-is-for-navbar-work/navbarData.js
@@ -0,0 +1,48 @@
+export const links = [
+    {
+        id: 1,
+        url: "./home",
+        text: "Home"
+    },
+    {
+        id: 2,
+        url: "./about",
+        text: "About"
+    },
+    {
+        id: 3,
+        url: "./projects",
+        text: "Projects"
+    },
+    {
+        id: 4,
+        url: "./contact",
+        text: "Contact"
+    }
+]
+
+export const social = [
+    {
+        id: 1,
+        url: "www.facebook.com",
+        icon: ""
+    },
+
+    {
+        id: 2,
+        url: "www.youtube.com",
+        icon: ""
+    },
+    {
+        id: 3,
+        url: "www.twitter.com",
+        icon: ""
+    },
+
+    {
+        id: 1,
+        url: "www.linkedin.com",
+        icon: ""
+    }
+
+]
